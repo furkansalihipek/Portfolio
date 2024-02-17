72c4f081bc1023e85bd71f845abae97be9529bd0
Furkan Salih  İpek <furkan.ipek@192.168.1.100>
2024-02-18 02:29:59 +0300

    page update and bug fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,69 @@
 export default function Home() {
   return (
-    <><header>
-      <nav className="flex justify-end mt-5">
-        <ul className="flex gap-20 mr-20">
-          <li className="text-primary"><a href="#">Home</a></li>
-          <li className="text-primary" s><a href="#">About</a></li>
-          <li className="text-primary" s><a href="#">Portfolio</a></li>
-          <li className="text-primary" s><a href="#">Reviews</a></li>
-          <li className="text-primary" s><a href="#">Services</a></li>
-          <li className="text-primary" s><a href="#">Contact</a></li>
-        </ul>
-      </nav>
-    </header><main className="h-screen">
-        <section>
-          <div className="flex justify-center items-center h-screen gap-96 pl-5 pr-5">
-            <div className="flex flex-col p-10 w-3/4 h-2/4">
-              <h2 className=" text-3xl">Furkan İPEK</h2>
-              <h1 className="font-bold mt-5 text-5xl text-primary">DEVELOPER</h1>
-              <p className="mt-5">Selam 👋 <br></br>Ben Furkan İPEK 20 yaşımdayım Burdur Mehmet Akif Ersoy Üniversitesinde Bilgisayar Programcılığı bölümünde okuyorum. İzmir’in Konak ilçesinde doğdum. Çocukluğumu ve üniversite dönemine kadar olan eğitim hayatımı İzmirde geçirdim. </p>
-            </div>
-            <div className="flex justify-center items-center w-1/4 h-2/4">
-              <img src="my-image.jpg" alt="Img" className="pr-20"/>
-            </div>
+    <div className="flex flex-col min-h-screen container mx-auto">
+      <header>
+        <div className="flex mt-5 justify-between items-center h-10">
+          <div className="pl-10 group">
+            <a href="#" className="font-bold text-primary group-hover:text-secondary">
+              <span className="group-hover:hidden text-5xl">&lt; &gt;</span>
+              <span className="hidden group-hover:inline text-3xl">&lt; Furkan İpek /&gt;</span>
+            </a>
           </div>
-        </section>
+          <ul className="flex gap-20 ml-auto">
+            <li><a href="#">Home</a></li>
+            <li className="opacity-50 group hover:opacity-100">
+              <a href="#" className="relative">
+                About
+                <span className="absolute -top-3 bg-primary py-1 px-2 rounded-full text-xs opacity-0 group-hover:opacity-100">Soon</span>
+              </a>
+            </li>
+            <li className="opacity-50 group hover:opacity-100">
+              <a href="#" className="relative">
+                Portfolio
+                <span className="absolute -top-3 bg-primary py-1 px-2 rounded-full text-xs opacity-0 group-hover:opacity-100">Soon</span>
+              </a>
+            </li>
+            <li className="opacity-50 group hover:opacity-100">
+              <a href="#" className="relative">
+                Services
+                <span className="absolute -top-3 bg-primary py-1 px-2 rounded-full text-xs opacity-0 group-hover:opacity-100">Soon</span>
+              </a>
+            </li>
+            <li className="opacity-50 group hover:opacity-100">
+              <a href="#" className="relative">
+                Contact
+                <span className="absolute -top-3 bg-primary py-1 px-2 rounded-full text-xs opacity-0 group-hover:opacity-100">Soon</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </header>
+
+      <main>
+        <div className="mt-20">
+          <main className=" flex justify-center items-center">
+            <section>
+              <div className="flex justify-center items-center gap-96">
+                <div className="flex flex-col p-10 w-3/4 h-2/4">
+                  <h2 className="text-3xl">Furkan İPEK</h2>
+                  <h1 className="font-bold mt-5 text-5xl text-primary">DEVELOPER</h1>
+                  <p className="mt-5">Selam 👋 <br></br>Ben Furkan İPEK. 20 yaşımdayım. Burdur Mehmet Akif Ersoy Üniversitesinde Bilgisayar Programcılığı bölümünde okuyorum. İzmir’in Konak ilçesinde doğdum. Çocukluğumu ve üniversite dönemine kadar olan eğitim hayatımı İzmirde geçirdim. </p>
+                  <div className="mt-20">
+                    <button className="w-28 h-20 bg-primary">Hire Me</button>
+                  </div>
+                </div>
+                <div className="flex justify-center items-center w-1/2 h-1/2">
+                  <img src="IMG_1227.jpg" alt="Img" className="border rounded-2xl border-primary" />
+                </div>
+              </div>
+            </section>
+          </main>
+        </div>
       </main>
-      <footer>
+
+      <footer className="mt-auto">
         <div className="flex gap-20 mb-5">
-          <div className="pl-16">
+          <div className="pl-11">
             <h2 className="font-bold mb-5">CONTACT</h2>
             <a href="mailto:furkan@ipek.dev">furkan@ipek.dev</a>
           </div>
@@ -36,7 +72,7 @@ export default function Home() {
             <a href="https://www.linkedin.com/in/furkansalihipek/">Linkedin</a>
           </div>
         </div>
-      </footer></>
-
+      </footer>
+    </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,14 +6,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-          "primary": "#9F7551",
-          "background": "#212121",
+      fontFamily: {
+        'm-plus-code': ['M PLUS Code Latin', 'sans-serif'],
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#9F7551",
+        background: "#212121",
       },
     },
   },
